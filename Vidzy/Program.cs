4706beaa56ecfc7d4edad4247d6a4137be7a13fb
hkk2016 <kerahari@gmail.com>
2020-05-24 20:12:24 +0530

    Assinment 1  (#3 Iterations)

--- Vidzy/Program.cs
+++ Vidzy/Program.cs
@@ -10,6 +10,12 @@ namespace Vidzy
     {
         static void Main(string[] args)
         {
+            using (VidzyEntities context = new VidzyEntities())
+            {
+                context.AddVideo("Lucy", DateTime.Now.AddYears(-3), "Thriller",(byte)Classification.Gold);
+                context.AddVideo("Bahubali 2", DateTime.Now.AddYears(-5), "Action",(byte)Classification.Platinum);
+                
+            }
         }
     }
 }
